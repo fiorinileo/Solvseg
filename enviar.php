1c2ea1550f3414a15a980b33d2072f322045388f
fiorinileo <leonardofiorini87@gmail.com>
2022-02-21 18:57:55 -0300

    primeras implementaciones de formulario de contacto

--- enviar.php
+++ enviar.php
@@ -171,6 +171,20 @@
     </main>
     <footer>
         <center>
+        <?php 
+            $myemail = 'leonardofiorini87@gmail.com';
+            $name = $_POST['nombre'];
+            $email = $_POST['email'];
+            $message = $_POST['mensaje'];
+
+            $to = $myemail;
+            $email_subject = "Nuevo mensaje: $subject";
+            $email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+            $headers = "From: $email";
+
+            mail($to, $email_subject, $email_body, $headers);
+            echo "El mensaje se ha enviado correctamente";
+            ?>
             <form action="enviar.php" method="post">
                 <input type="text" name="nombre" placeholder="NOMBRE">
                 <input type="email" name="email" id="CORREO">
--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ window.addEventListener('load', function(){
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		scrollLock: true,
 		dots: '.carousel__indicadores',
 		arrows: {
 			prev: '.carousel__anterior',
