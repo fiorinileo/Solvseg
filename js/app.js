c4571f095b4be42f478a395be81eaf477ec2f76e
fiorinileo <leonardofiorini87@gmail.com>
2022-02-22 23:43:28 -0300

    implementación de javascript para controlar el despliegue del submenu

--- js/app.js
+++ js/app.js
@@ -6,6 +6,26 @@ const navbar = document.querySelector("#main-navbar");
 navbarToggle.addEventListener("click", ()=>{
     navbar.classList.toggle('navbar-open');
 });
+
+const subMenuBtn = document.querySelectorAll(".submenu-btn");
+for(let i=0;i<subMenuBtn.length;i++){
+	subMenuBtn[i].addEventListener("click", function(){
+		if(window.innerWidth < 920){
+			const subMenu = this.nextElementSibling;
+			const height = subMenu.scrollHeight;
+
+			if(subMenu.classList.contains("desplegar")){
+				subMenu.classList.remove("desplegar");
+				subMenu.removeAttribute("style");
+			}else{
+				subMenu.classList.add("desplegar");
+				subMenu.style.height = height + "px";
+			}
+		}
+	});
+}
+
+
 window.addEventListener('load', function(){
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
