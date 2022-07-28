0cd8dd1a42e9a74a7904e7dc147b9e7d7b676b44
fiorinileo <leonardofiorini87@gmail.com>
2022-07-28 19:15:47 -0300

    implementacion de modal a servicios

--- js/app.js
+++ js/app.js
@@ -14,3 +14,36 @@ navbar.addEventListener("click", () => {
 
 
 
+/* -----------------------MODAL-------------------*/
+if(document.getElementById("btn-modal-cctv")){
+	var modal = document.getElementById("myModal");
+	var btn = document.getElementById("btn-modal-cctv");
+	var span = document.getElementsByClassName("close")[0];
+	var body = document.getElementsByTagName("body")[0];
+
+	btn.onclick = function() {
+		modal.style.display = "block";
+
+		body.style.position = "static";
+		body.style.height = "100%";
+		body.style.overflow = "hidden";
+	}
+
+	span.onclick = function() {
+		modal.style.display = "none";
+
+		body.style.position = "inherit";
+		body.style.height = "auto";
+		body.style.overflow = "visible";
+	}
+
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+
+			body.style.position = "inherit";
+			body.style.height = "auto";
+			body.style.overflow = "visible";
+		}
+	}
+}
