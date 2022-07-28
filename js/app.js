1e7cf9dab0a7e1ad2e8f2c48f1aa3512bf39f424
fiorinileo <leonardofiorini87@gmail.com>
2022-07-28 16:30:34 -0300

    ajuste de navbar

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
 //nav Toggle - select button and links
 const navbarToggle = document.querySelector("#navbarToggle");
 const navbar = document.querySelector("#main-navbar");
-
+const servicesLink= document.getElementById("navbar__link--services");
 //add event listener
 navbarToggle.addEventListener("click", ()=>{
     navbar.classList.toggle('navbar-open');
 });
-
-const subMenuBtn = document.querySelectorAll(".submenu-btn");
-for(let i=0;i<subMenuBtn.length;i++){
-	subMenuBtn[i].addEventListener("click", function(){
-		if(window.innerWidth < 920){
-			const subMenu = this.nextElementSibling;
-			const height = subMenu.scrollHeight;
-
-			if(subMenu.classList.contains("desplegar")){
-				subMenu.classList.remove("desplegar");
-				subMenu.removeAttribute("style");
-			}else{
-				subMenu.classList.add("desplegar");
-				subMenu.style.height = height + "px";
-			}
-		}
-	});
+navbar.addEventListener("click", () => {
+	if(navbar.classList.contains('navbar-open')){
+	navbar.classList.remove('navbar-open');
 }
+})
+
+
 
