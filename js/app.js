42eb5f18e81d6b618c56f773ae9641dec401c26a
fiorinileo <leonardofiorini87@gmail.com>
2022-08-14 08:05:20 -0300

    retoques en el js

--- js/app.js
+++ js/app.js
@@ -48,39 +48,3 @@ function abrirModal(seccion,indice){
 		}
 	}
 }
-
-/* 
-if(document.getElementById("btn-modal-cctv")){
-	
-	var modal = document.getElementById("Modal-cctv");
-	var btn = document.getElementById("btn-modal-cctv");
-	var span = document.getElementsByClassName("close")[0];
-	var body = document.getElementsByTagName("body")[0];
-
-	btn.onclick = function () {
-		modal.style.display = "block";
-
-		body.style.position = "static";
-		body.style.height = "100%";
-		body.style.overflow = "visible";
-	}
-
-	span.onclick = function() {
-		modal.style.display = "none";
-
-		body.style.position = "inherit";
-		body.style.height = "auto";
-		body.style.overflow = "visible";
-	}
-
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-
-			body.style.position = "inherit";
-			body.style.height = "auto";
-			body.style.overflow = "visible";
-		}
-	}
-}
- */
